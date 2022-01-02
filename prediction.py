947bfcad86548625627977350aac67c8e8c67ed4
Dongliang (Larry) Yi <dongliang.yi@aol.com>
2022-01-01 21:54:53 -0800

    by default, it has a file

--- prediction.py
+++ prediction.py
@@ -2,21 +2,24 @@ import streamlit as st
 import numpy as np
 from io import StringIO
 import pandas as pd
+import joblib
+import pickle
 
 st.title('Prediction of repayment')
 
 uploaded_file = st.file_uploader("Choose a file)")
+input_data = pd.read_csv("X_test_final.csv")
+st.write("By default, the file that runs is X_test_final.csv")
+
 if uploaded_file is not None:
      # Can be used wherever a "file-like" object is accepted:
      input_data = pd.read_csv(uploaded_file)
      #st.write(dataframe)
 
-# Save the random forest model
-import joblib
+# Retrieve the random forest model
 loaded_model = joblib.load("classifier.pkl")
 
 # retrieve the list of selected variables
-import pickle
 with open("selected_features.txt", "rb") as fp:   # Unpickling
     b = pickle.load(fp)
 
