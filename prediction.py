86749a872be616a7752ad136843b4ad846c972fc
Dongliang (Larry) Yi <dongliang.yi@aol.com>
2022-01-01 22:03:56 -0800

    change file names

--- prediction.py
+++ prediction.py
@@ -8,7 +8,7 @@ import pickle
 st.title('Prediction of repayment')
 
 uploaded_file = st.file_uploader("Choose a file)")
-input_data = pd.read_csv("X_test_final Full.csv")
+input_data = pd.read_csv("X_test_final.csv")
 st.write("By default, the file that runs is X_test_final.csv")
 if uploaded_file is not None:
      # Can be used wherever a "file-like" object is accepted:
@@ -34,9 +34,9 @@ st.write("""
 ID_No = 1
 for x in y_pred:
     if x == 0:
-        st.write('Borrower #', ID_No, "is expected to repay the loan")
+        st.write('Applicant #', ID_No, "is expected to repay the loan")
     else:
-        st.write('WARNING!!! Borrower #', ID_No, "is expected to default the loan" )
+        st.write('WARNING!!! Applicant #', ID_No, "is expected to default the loan" )
     ID_No = ID_No + 1
 
 
